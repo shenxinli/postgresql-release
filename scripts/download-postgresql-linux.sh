aa03ee1c3b48e1f5a56f04314971e4d648d8fd8e
shenxinli <shenxinli@163.com>
2025-05-11 12:45:40 +0800

    bb

--- scripts/download-postgresql-linux.sh
+++ scripts/download-postgresql-linux.sh
@@ -27,30 +27,6 @@ if [[ "$ARCH" == "amd64" ]]; then
   echo "Downloading $FILENAME from $URL"
   curl -fSL "$URL" -o "postgresql-linux-amd64/${FILENAME}"
   echo "Download completed: postgresql-linux-amd64/${FILENAME}"
-
-elif [[ "$ARCH" == "arm64" ]]; then
-  echo "Building PostgreSQL $VERSION from source for linux/arm64..."
-
-  # Install dependencies
-  sudo apt-get update
-  sudo apt-get install -y build-essential gcc-aarch64-linux-gnu libreadline-dev zlib1g-dev flex bison wget tar
-
-  # Download source
-  wget https://ftp.postgresql.org/pub/source/v${FULL}/postgresql-${FULL}.tar.gz
-  tar -xzf postgresql-${FULL}.tar.gz
-  mv postgresql-${FULL} postgresql-${VERSION} && cd postgresql-${VERSION}
-
-  OUTPUT_DIR=$(pwd)/output
-  mkdir -p "$OUTPUT_DIR"
-  CC=aarch64-linux-gnu-gcc ./configure --prefix=${OUTPUT_DIR} --enable-debug --with-openssl --without-readline --without-zlib
-  make -j$(nproc)
-  make install
-
-  # Package the result
-  ls -l ${OUTPUT_DIR}
-  echo "compress build: postgresql-${VERSION}-linux-${ARCH}.tar.gz in ${OUTPUT_DIR}"
-  tar -czf postgresql-${VERSION}-linux-${ARCH}.tar.gz ${OUTPUT_DIR}
-  echo "Build and packaging complete: postgresql-${VERSION}-linux-${ARCH}.tar.gz"
 else
   echo "Unsupported architecture: $ARCH"
   exit 1
